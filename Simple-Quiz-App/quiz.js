687b75de03fdfda2692aea1f45690410a9900f49
Komal Bhise <komalbhise49@gmail.com>
2024-03-13 16:55:43 +0530

    Added Main Files

---
+++ Simple-Quiz-App/quiz.js
@@ -0,0 +1,506 @@
+//array for Topic Ancient India .
+let quizDB1 =[
+    {   id: 1,
+        question: "1.The University of Nalanda was set up by which Gupta ruler?",
+        a:"[A] Karnataka",
+        b:"[B] Gujarath",
+        c:"[C] Tamilnadu",
+        d:"[D] Kerala",
+        ans:"ans1"
+    },
+    {   id: 2,
+        question: "2.Which of the following was the birth place of Guru Nanak ?",
+        a:"[A] Shakargarh",
+        b:"[B] Sultanpur",
+        c:"[C] Nankana Sahib",
+        d:"[D] Shahkot",
+        ans:"ans3"
+    },{ id: 3,
+        question: "3.Who among the following preached the doctrine of One religion, one caste and one God for mankind?",
+        a:"[A] Jyotiba Phule",
+        b:"[B] Vivekananda",
+        c:"[C] Sri Narayan Guru",
+        d:"[D] Dr. B. R. Ambedkar",
+        ans:"ans3"
+    },{ id: 4,
+        question: "4.Singhey Khababs Festival is celebrated in which among the following states of India?",
+        a:"[A] Himachal Pradesh",
+        b:"[B] Jammu & Kashmir",
+        c:"[C] Uttarakhand",
+        d:"[D] Punjab",
+        ans:"ans2"
+    },{ id: 5,
+        question: "5.The terms Udara, Mudara and Tara are related to which among the following as per the ancient Indian texts?",
+        a:"[A] Medicines",
+        b:"[B] Gods",
+        c:"[C] Music",
+        d:"[D] Millitary science",
+        ans:"ans3"
+    },{ id: 6,
+        question: "6.In field of Music, which among the following state Governments gives Tana Riri Award every year?",
+        a:"[A] Rajasthan",
+        b:"[B] Madhya Pradesh",
+        c:"[C] Gujarat",
+        d:"[D] Maharashtra",
+        ans:"ans3"
+    },{ id: 7,
+        question: "7.Who among the following produced India’s first dubbed film, Harischandra, in 1944?",
+        a:"[A] A. V. Meiyappan",
+        b:"[B] S. S. Vasan",
+        c:"[C] L V Prasad",
+        d:"[D] Kadaru Nagabhushanam",
+        ans:"ans1"
+    },{ id: 8,
+        question: "8.Which period is regarded as the Golden age of Telugu literature?",
+        a:"[A] Satavahana rule",
+        b:"[B] Eastern Chalukyan rule",
+        c:"[C] Vijayanagara rule",
+        d:"[D] Kakatiya rule",
+        ans:"ans3"
+    },{ id: 9,
+        question: "9.Kundhei puppetry art belongs to which of the following states?",
+        a:"[A] West Bengal",
+        b:"[B] Odisha",
+        c:"[C] Karnataka",
+        d:"[D] Kerala",
+        ans:"ans2"
+    },{ id: 10,
+        question: "10.Which of the following is considered as the major quality of Lord Buddha?",
+        a:"[A] Anuttaro Purisa-damma-sarathi",
+        b:"[B] Satthadeva-Manussanam",
+        c:"[C] Bhagavathi",
+        d:"[D] All of the above",
+        ans:"ans4"
+    },
+]
+//General Science question Array .
+let quizDB2 =[
+    {   id: 1,
+        question: "1.Which of the following is an alkaline earth metal?",
+        a:"[A] Pottasium",
+        b:"[B] Lead",
+        c:"[C] Tamilnadu",
+        d:"[D] Copper",
+        ans:"ans4"
+    },
+    {   id: 2,
+        question: "2.Which among the following is a millet?",
+        a:"[A] Sorghum",
+        b:"[B] Maize",
+        c:"[c] Rice",
+        d:"[C] Wheat",
+        ans:"ans1"
+    },{ id: 3,
+        question: "3.A pollen tube is used in the transport of..?",
+        a:"[A] Pollen Grains",
+        b:"[B] Sperm Cells",
+        c:"[C] Eggs",
+        d:"[D] All of them",
+        ans:"ans2"
+    },{ id: 4,
+        question: "4.Gout which is usually followed / accompanied by arthritis are characterized by high levels of uric acid in the blood and uric acid crystals become lodged in the joints. It is a _ _ _?",
+        a:"[A] Viral Infection",
+        b:"[B] Metabolic Disorder",
+        c:"[C] Muscle Disorder",
+        d:"[D] Bacterial infection",
+        ans:"ans2"
+    },{ id: 5,
+        question: "5.How many vertebrae does a human being have ?",
+        a:"[A] 33",
+        b:"[B] 36",
+        c:"[C] 29",
+        d:"[D] 19",
+        ans:"ans1"
+    },{ id: 6,
+        question: "6.Which one of the following is responsible for maintenance of osmotic pressure in blood?",
+        a:"[A] Fibrinogen",
+        b:"[B] Red Blood Cells",
+        c:"[C] Albumin",
+        d:"[D] Platelets",
+        ans:"ans3"
+    },{ id: 7,
+        question: "7.What is the time taken by the earth to complete one rotation about its axis with regard to a fixed star?",
+        a:"[A] Sidereal day",
+        b:"[B] Solar day",
+        c:"[C] Shake",
+        d:"[D] Tropical year",
+        ans:"ans1"
+    },{ id: 8,
+        question: "8.Work done is defined as the dot product of which of the following vectors?",
+        a:"[A] Force and acceleration",
+        b:"[B] Force and area",
+        c:"[C] Force and instantaneous velocity",
+        d:"[D] Force and displacement",
+        ans:"ans4"
+    },{ id: 9,
+        question: "9.What kind of energy is stored in the spring of a watch?",
+        a:"[A] Kinetic",
+        b:"[B] Odisha",
+        c:"[C] Heat",
+        d:"[D] Chemical",
+        ans:"ans2"
+    },{ id: 10,
+        question: "10.Which of the following techniques can be used to separate cream from milk?",
+        a:"[A] Filtration",
+        b:"[B] Distillation",
+        c:"[C] Centrifugation",
+        d:"[D] Chromatography",
+        ans:"ans3"
+    },
+];
+//Modern India Array .
+let quizDB3 =[
+    {   id: 1,
+        question: "1.Which among the following Congress sessions was known for concession of separate electorates for the Muslims by the Congress Party?",
+        a:"[A] 1913 Karachi Session",
+        b:"[B] 1915 Bombay Session",
+        c:"[C] 1916 Lucknow Session",
+        d:"[D] 1917 Calcutta Session",
+        ans:"ans4"
+    },
+    {   id: 2,
+        question: "2.President of Indian National Congress at the time of independence was___?",
+        a:"[A] J. B. Kripalani",
+        b:"[B] Maulana Abul Kalam Azad",
+        c:"[C] Dr. Rajendra Prasad",
+        d:"[D] Jawahar Lal Nehru",
+        ans:"ans1"
+    },{ id: 3,
+        question: "3.In which year, Partition of Bengal was annulled?",
+        a:"[A] Lord Reading",
+        b:"[B] Lord Irwin",
+        c:"[C] Lord Linlithgow",
+        d:"[D] Lord wavel",
+        ans:"ans2"
+    },{ id: 4,
+        question: "4.Who among the following founded “National Herald”?",
+        a:"[A] Bal Gangadhar Tilak",
+        b:"[B] Mahatma Gandhi",
+        c:"[C] Jawahar Lal Nehru",
+        d:"[D] Indira Gandhi",
+        ans:"ans3"
+    },{ id: 5,
+        question: "5.The dual government of Bengal was result of a farman in 1765 by which of the following ?",
+        a:"[A] British",
+        b:"[B] Shah Alam",
+        c:"[C] Nawab Sirajuddaula",
+        d:"[D] Raja Khan",
+        ans:"ans2"
+    },{ id: 6,
+        question: "6.For which of the following commodities , the Dutch wanted to barter cotton piece goods in the Indonesian Archipelago ?",
+        a:"[A] Tobacco",
+        b:"[B] Rice",
+        c:"[C] Pepper and Spices",
+        d:"[D] Silk",
+        ans:"ans3"
+    },{ id: 7,
+        question: "7Which of the following acts first time explicitly defined the constitutional position of the British territories in India ?",
+        a:"[A] The Regulating Act of 1773",
+        b:"[B] The Charter Act of 1813",
+        c:"[C] The Charter Act of 1833",
+        d:"[D] Act of 1858",
+        ans:"ans2"
+    },{ id: 8,
+        question: "8.Who among the following repealed the Vernacular Press Act ?",
+        a:"[A] Lord Lytton",
+        b:"[B] Lord Ripon",
+        c:"[C] Lord Dufferin",
+        d:"[D] Lord Lansdowne",
+        ans:"ans2"
+    },{ id: 9,
+        question: "9. Who among the following remain loyal to British during the Revolt of 1857?",
+        a:"[A] Holkar of Indore",
+        b:"[B] Gulab Singh of Kashmir",
+        c:"[C] Man Singh of Gwalior",
+        d:"[D] All of the above",
+        ans:"ans4"
+    },{ id: 10,
+        question: "10. Which city has topped the Mercer Quality of Living Survey 2018 ?",
+        a:"[A] Zurich",
+        b:"[B] Hyderabad",
+        c:"[C] Munich",
+        d:"[D] Vienna",
+        ans:"ans4"
+    },
+];
+//Indian Geography question .
+let quizDB4 =[
+    {   id: 1,
+        question: "1.Polavaram Project is located in which state?",
+        a:"[A] Madhya Pradesh",
+        b:"[B] Gujarat",
+        c:"[C] Andhra Pradesh",
+        d:"[D] Karnataka",
+        ans:"ans3"
+    },
+    {   id: 2,
+        question: "2.Which type of forests occupies the highest percentage of area in India ?",
+        a:"[A] Tropical deciduous",
+        b:"[B] Savanna and desert vegetation",
+        c:"[C] Equatorial evergreen",
+        d:"[D] Tropical Rainforests",
+        ans:"ans1"
+    },{ id: 3,
+        question: "3.Cooch Behar in West Bengal is famous for which among the following industries?",
+        a:"[A] Cotton Cloth Industry",
+        b:"[B] Silk Industry",
+        c:"[C] Petroleum Industry",
+        d:"[C] Petroleum Industry",
+        ans:"ans2"
+    },{ id: 4,
+        question: "4.In which five year plan Rourkela & Durgapur Steel plants were conceived?",
+        a:"[A] First",
+        b:"[B] Second",
+        c:"[C] Third",
+        d:"[D] Fourth",
+        ans:"ans2"
+    },{ id: 5,
+        question: "5.The east west corridor of national Highways Development Project is connects _______?",
+        a:"[A] Guwahati to Ahmedabad",
+        b:"[B] Silchar to Porbandar",
+        c:"[C] Guwahati to Junagarh",
+        d:"[D] Guwahati to Delhi",
+        ans:"ans2"
+    },{ id: 6,
+        question: "6.Which of the following represents largest fraction of forest cover in India?",
+        a:"[A] Tropical Dry deciduous",
+        b:"[B] Tropical moist Deciduous",
+        c:"[C] Tropical Evergreen",
+        d:"[D] Sub tropical",
+        ans:"ans2"
+    },{ id: 7,
+        question: "7.Which among the following is a riverine (Inland River) port?",
+        a:"[A] Chennai",
+        b:"[B] Kandla",
+        c:"[C] Kolkata",
+        d:"[D] Tutikorin",
+        ans:"ans3"
+    },{ id: 8,
+        question: "8.Best quality coal is found in which of the following system?",
+        a:"[A] Vindhyan System",
+        b:"[B] Cuddapah System",
+        c:"[C] Gondwana System",
+        d:"[D] Dharwar System",
+        ans:"ans3"
+    },{ id: 9,
+        question: "9. Which among the following is the largest physiographic division of India?",
+        a:"[A] Peninsular Upland",
+        b:"[B] Himalayan Region",
+        c:"[C] Northern plains",
+        d:"[D] Desert region",
+        ans:"ans1"
+    },{ id: 10,
+        question: "10. Which of the following is the highest point of Meghalaya?",
+        a:"[A] Shilong",
+        b:"[B] Nokrek",
+        c:"[C] Lum Shyllong",
+        d:"[D] Marangksih",
+        ans:"ans1"
+    },
+]
+//user name ref.
+ const msg = document.getElementById("msg");//instruction for user to enter a name .
+ const enter = document.getElementById("enter");
+ const category = document.getElementById("category");//category reference.
+ const home = document.getElementById("home");//main Page reference.
+//adding Different Array in Various Element .
+let quizDB =[];
+//Reference for selection Topic .
+const ancientIndia = document.getElementById("ancientIndia");
+const generalScience = document.getElementById("generalScience");
+const mordernIndia =document.getElementById("mordernIndia");
+const geography = document.getElementById("geography");
+//getting references .
+const queNo = document.getElementById("queNo")
+const question = document.getElementById("question");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
+const submit = document.getElementById("submit");
+const answers = document.querySelectorAll(".answer");
+const timesup = document.getElementById("timesup");;
+const list = document.getElementById("list");
+const reload = document.getElementById("startagain");
+const gohome = document.getElementById("gotohome");
+const ancient = document.getElementById("ancient");
+const result = document.getElementById("result");
+const totalTime = document.getElementById("totalTime");
+const wrongScore = document.getElementById("wrongScore"); //work in progress .
+const correctScore = document.getElementById("correctScore");
+const Attempt = document.getElementById("attempt");
+const percentage = document.getElementById("percentage");
+const timeScore = document.getElementById("time&score");
+const pannel = document.getElementById("pannel");
+const Home = document.getElementById("home") ;//this is for starting page of quiz .
+const topicName = document.getElementById("topicName"); 
+
+//counting Totaltime .
+let displaytimeAll = 0
+let score = 0;//counting score .
+//for changing a question variable is delclared.
+let questionCount = 0 ;//variable for adding next question .
+let timming = 0 ; //variable for countion a 10sec time .
+let myTime ;//set interval for que timer
+let allTimer = 0 ; //full time counter
+let wrong = 0 ;//counting wrong question .
+let answer; //variable for checkd answer .
+
+//loading Question .
+const loadQuestion = () =>{
+    home.style.display = "none";
+    category.style.display = "none";
+    const questionList = quizDB[questionCount];
+    question.innerHTML= questionList.question;
+    queNo.innerHTML = questionList.id + "/10"; 
+
+    option1.innerHTML = questionList.a ;
+    option2.innerHTML = questionList.b ;
+    option3.innerHTML = questionList.c ;
+    option4.innerHTML = questionList.d ;
+
+  timming = 0 ;//counting 10 sec timer .
+for(i = 0 ; i<1; i++){
+   function timer(){
+     let displaytime = timming+=i ;
+      document.getElementById("timer").innerHTML = displaytime;
+       if(displaytime == 10){
+        clearInterval( myTime)
+        timesup.style.display = "block";
+        timesup.innerHTML = "Times Up 😥";
+        list.style.display = "none";
+
+         }
+        }
+      };
+  myTime= setInterval(timer , 1000) ;
+    }
+//Counting Total Time ;
+allTimer = 0 ;
+for(i = 0 ; i<1; i++){
+   function Alltimer(){
+     displaytimeAll = allTimer +=i ;
+      totalTime.innerHTML ="Total Time Taken ⏰ :"+" "+ displaytimeAll+ "seconds";
+    }
+      };
+  let AllTime= setInterval(Alltimer , 1000) ;
+    //function for answer is correct or not.
+const getcheckAnswer = ()=>{
+
+    answers.forEach((curAnsElem)=>{
+        if(curAnsElem.checked){
+            answer = curAnsElem.id ;
+
+        }
+    });
+     return answer
+    }
+//function for delesect .
+const deselectop =()=> {
+    answers.forEach((curAnsElem)=> curAnsElem.checked = false);
+    }
+//submit button for different events .
+submit.addEventListener("click",()=>{
+    timesup.style.display = "none";
+    list.style.display = "block";
+    const checkedAnswer = getcheckAnswer ();
+
+    if(checkedAnswer === quizDB[questionCount].ans){
+        score++ ;
+        document.getElementById("score").innerHTML = score ;
+        correctScore.innerHTML = "Your score 🥳 :"+" " + score;
+    }else{
+        wrong++
+        wrongScore.innerHTML ="wrong ☹️ :"+" "+ wrong  ;
+    };
+     deselectop();
+    timming = 0 ;
+    clearInterval( myTime)
+    questionCount++ ;
+    if(questionCount < quizDB.length){
+        loadQuestion();
+    }else{ancient.style.display = "none";
+    result.style.display = "block"
+    clearInterval( AllTime)
+    let attempt = parseInt(score) + parseInt(wrong) 
+    Attempt.innerHTML ="Attempt 👉:"+" " + attempt ;
+    let per = parseInt(score)/10*100;
+    percentage.innerHTML ="Percentage 😎 :"+" "+ per + "%"
+    timeScore.style.display = "none";
+    let U_name = document.getElementById("abc").value;
+    document.getElementById("r_name").innerHTML = U_name + " "+"Your Result is :"
+
+}//end of else statement
+
+
+});//end of submit button .
+//reloading a quiz .
+gohome.addEventListener("click",()=>{
+    location.reload();
+    });
+//loading another Topic
+ancientIndia.addEventListener("click",()=>{
+    quizDB = quizDB1;
+    pannel.style.display = "block" ;
+    loadQuestion();
+    topicName.innerHTML = "Ancient India";
+
+    }) ;//ancient India End .
+generalScience.addEventListener("click",()=>{
+    quizDB = quizDB2;
+    pannel.style.display = "block" ;
+    loadQuestion();
+    topicName.innerHTML = "General Knowledge";
+});
+mordernIndia.addEventListener("click",()=>{
+    quizDB = quizDB3;
+    pannel.style.display = "block" ;
+    loadQuestion();
+    topicName.innerHTML = "Modern India" ;
+});
+geography.addEventListener("click",()=>{
+    quizDB = quizDB4;
+    pannel.style.display = "block" ;
+    loadQuestion();
+    topicName.innerHTML = "Indian Geography" ;
+});
+
+//Enter u_name and start name .
+enter.addEventListener("click",()=>{
+    let U_name = document.getElementById("abc").value;
+    if(U_name===""){
+        msg.style.border = "4px solid red"
+        msg.innerHTML = "Dear User please Enter Your Good Name !";
+         }
+    else{
+    msg.style.border = "4px solid black"
+    msg.innerHTML ="Hello "+ U_name + " "+"Lets Play Quiz !";
+    category.style.display = "block";};
+    });
+//reload the quiz .
+reload.addEventListener("click",()=>{ //reload Function .
+    //ancient.style.display = "block"
+    questionCount = 0 ;
+      score = 0;
+      displaytimeAll = 0 ;
+
+     //allTimer = 0 ; 
+       wrong = 0 ;
+    ancient.style.display = "block"
+    result.style.display = "none";
+    pannel.style.display = "block";
+    timeScore.style.display = "block";
+    //document.getElementById("score").innerHTML = score ;
+const questionList = quizDB[questionCount];
+question.innerHTML= questionList.question;
+queNo.innerHTML = questionList.id + "/10"; 
+
+option1.innerHTML = questionList.a ;
+option2.innerHTML = questionList.b ;
+option3.innerHTML = questionList.c ;
+option4.innerHTML = questionList.d ;
+document.getElementById("score").innerHTML = score ;
+
+loadQuestion();
+ });
+ //End of script . 
